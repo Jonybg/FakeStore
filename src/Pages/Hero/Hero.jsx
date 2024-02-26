2e09b85eb6e056e342ab1201420561d8487c7d49
jonathanbello123 <jonathanbellogabriel@gmail.com>
2024-02-26 12:20:08 -0300

    secondcommit

--- src/Pages/Hero/Hero.jsx
+++ src/Pages/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
                 <img className="hidden xl:block w-full h-full absolute inset-0" src={BG} alt="" />
                 <div className="absolute top-1/2 left-0 ml-2 lg:ml-0 lg:left-1/4 transform -translate-y-1/2">
                     <h1 className="text-black xl:text-4xl text-xl font-bold mb-4">Discover the Latest Trends</h1>
-                    <p className="text-black xl:text-lg text-[11px] mb-6">Explore our collection of high-quality clothing and electronics.</p>
+                    <p className="text-black xl:text-lg  text-[11px] mb-6">Explore our collection of high-quality clothing and electronics.</p>
                     <button className="px-6  xl:px-8 py-2 xl:py-3 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300 ease-in-out">Explore Now</button>
                 </div>
             </div>
